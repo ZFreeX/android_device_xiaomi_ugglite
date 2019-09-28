294c887a15e88864f0d0f776f5da1f94dd691afe
ZFreeX <fkm505@mail.ru>
2019-09-28 19:16:33 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# android_device_xiaomi_ugglite
-Device tree (ugglite) for pie roms.
+(C) The LineageOS Project 2019
+
+Xiaomi Redmi Note 5A / Y1 Lite | Codename: _"ugglite"_
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Quad-core 1.4 GHz Cortex-A53
+CHIPSET | Qualcomm MSM8937 Snapdragon 425
+GPU     | Adreno 308
+Memory  | 2 GB
+Shipped Android Version | Android 7.1.2 with MIUI 8
+Storage | 16 GB
+MicroSD | Up to 256 GB (Hybrid)
+Battery | 3080 mAh (non-removable)
+Dimensions | 153 x 76.2 x 7.6 mm
+Display | 720 x 1280 pixels, 5.5" IPS
+Rear Camera  | 13.0 MP, LED flash
+Front Camera | 5.0 MP
+Release Date | November 2017
+
+![Xiaomi Redmi Note 5A / Y1 Lite](https://souqcms.s3.amazonaws.com/spring/images/2017/Xiaomi/Redmi-Note-5A-Dual-Sim/4-Redmi-Note-5A-Dual-Sim-Grey.jpg "Xiaomi Redmi Note 5A/Y1 Lite")
---
+++ bootleg_ugglite.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#               2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+
+# Inherit from ugglite device
+$(call inherit-product, device/xiaomi/ugglite/device.mk)
+
+PRODUCT_NAME := bootleg_ugglite
+PRODUCT_DEVICE := ugglite
+PRODUCT_MODEL := Redmi Note 5A
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := ugglite
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="ugglite" \
+    PRODUCT_NAME="ugglite" \
+    
+# Enforce privapp-permissions whitelist
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=log
+
+PRIVATE_BUILD_DESC="ugglite-user 7.1.2 N2G47H V9.1.2.0.NDFMIEI release-keys"
+
+# Set BUILD_FINGERPRINT variable
+BUILD_FINGERPRINT := "Xiaomi/ugglite/ugglite:7.1.2/N2G47H/V9.1.2.0.NDFMIEI:user/release-keys"
+
+BOOTLEGGERS_BUILD_TYPE := Unshishufied
+DEVICE_MAINTAINERS := adislice
